8dff8282b4cb9916912f4f5df770d73c88ed23f7
SridharLokesh <153600925+SridharLokesh@users.noreply.github.com>
2024-06-12 00:47:00 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer class="text-light">
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-6">
+            <a class="navbar-brand" href="index.php"><i class="flaticon-dog-20"></i><span>Metazoa A-z!</span></a>
+            <p class="mt-3">Our Shop is very loyal and low cost prices in every pets. so, u will believed in shop and find your new friend.</p>
+         </div>
+		 <!--/ col-lg -->
+         <div class="col-lg-3">
+            <h6><i class="fas fa-envelope margin-icon"></i>Contact Us</h6>
+            <ul class="list-unstyled">
+               <li>(123) 456-789</li>
+               <li><a href="mailto:email@yoursite.com">metazoa@gmail.com</a></li>
+               <li>T.Nagar, Chennai </li>
+            </ul>
+			<!--/ul -->
+         </div>
+		  <!--/ col-lg -->
+         <div class="col-lg-3">
+            <h6><i class="far fa-clock margin-icon"></i>Working Hours</h6>
+            <ul class="list-unstyled">
+               <li>Open 9am - 10pm</li>
+               <li>Holidays - Closed</li>
+               <li>Weekends - Closed</li>
+            </ul>
+			<!--/ul -->
+         </div>
+		 <!--/ col-lg -->
+      </div>
+	  <!--/ row-->
+     
+	  <!--/ row -->
+   </div>
+   <!--/ container -->
+   <!-- Go To Top Link -->
+   <div class="page-scroll hidden-sm hidden-xs">
+      <a href="#top" class="back-to-top"><i class="fa fa-angle-up"></i></a>
+   </div>
+   <!--/page-scroll-->
+</footer>
---
+++ header.php
@@ -0,0 +1,124 @@
+<nav id="main-nav" class="navbar-expand-xl fixed-top">
+         <div class="row">
+            <!-- Start Top Bar -->
+            <div class="container-fluid top-bar">
+               <div class="container">
+                  <div class="row">
+                     <div class="col-md-12">
+                        <!-- Start Contact Info -->
+                        <ul class="contact-details float-left">
+                           <li><i class="fa fa-map-marker"></i>Pet Street 123 - Chennai</li>
+                           <li><i class="fa fa-envelope"></i><a href="mailto:email@site.com">petshop@gmail.com</a></li>
+                           <li><i class="fa fa-phone"></i>(123) 456-789</li>
+                        </ul>
+                        <!-- End Contact Info -->
+                        <!-- Start Social Links -->
+                        <ul class="social-list float-right list-inline">
+                           <li class="list-inline-item"><a title="Facebook" href="#"><i class="fab fa-facebook-f"></i></a></li>
+                           <li class="list-inline-item"><a title="Twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                           <li class="list-inline-item"><a  title="Instagram" href="#"><i class="fab fa-instagram"></i></a></li>								
+                        </ul>
+                        <!-- /End Social Links -->
+                     </div>
+					  <!-- col-md-12 -->
+                  </div>
+				   <!-- /row -->
+               </div>
+			    <!-- /container -->
+            </div>
+            <!-- End Top bar -->
+            <!-- Navbar Starts -->
+            <div class="navbar container-fluid">
+               <div class="container ">
+			     <!-- logo -->
+                  <a class="navbar-brand" href="index.php">
+					  <i class="flaticon-dog-20"></i><span>Metazoa A-Z</span>
+                 <p>Pet World</p>
+				  </a>
+                 <!-- Navbartoggler -->
+				  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggle-icon">
+					 <i class="fas fa-bars"></i>
+					</span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarResponsive">
+                     <ul class="navbar-nav ml-auto">
+					  <!-- menu item -->
+                        <li class="nav-item">
+                           <a class="nav-link" href="index.php">Home
+                           </a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" id="services-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           Food&Accessories
+                           </a>
+                           <div class="dropdown-menu" aria-labelledby="services-dropdown">
+                              <a class="dropdown-item" href="dogfood.php">Dog</a>
+                              <a class="dropdown-item" href="catfood.php">Cat</a>
+                              <a class="dropdown-item" href="birdfood.php">Bird</a>
+                              <a class="dropdown-item" href="rabbitfood.php">Rabbit</a>
+                              <a class="dropdown-item" href="goatfood.php">Goat</a>
+                           </div>
+                        </li>
+						  
+						  <!-- menu item -->
+                    <!--     <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" id="about-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           About
+                           </a>
+                           <div class="dropdown-menu" aria-labelledby="about-dropdown">
+                              <a class="dropdown-item" href="about.php">About Style 1</a>
+                              <a class="dropdown-item" href="about2.php">About Style 2</a>
+                           </ -->
+                        </li>					
+						  <!-- menu item -->
+                        <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" id="adopt-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           Pet
+                           </a>
+                           <div class="dropdown-menu" aria-labelledby="adopt-dropdown">
+                              <a class="dropdown-item" href="adoption-2.php">DOG</a>
+                              <a class="dropdown-item" href="adoptioncat.php">CAT</a>
+                              <a class="dropdown-item" href="adoptionbird.php">BIRD</a><a class="dropdown-item" href="adoptionrabbit.php">RABBIT</a><a class="dropdown-item" href="adoptiongoat.php">GOAT</a>
+                           </div>
+                        </li>
+						  <!-- menu item -->
+                       
+						  <!-- menu item -->
+                        <li class="nav-item dropdown">
+                           <a class="nav-link dropdown-toggle" href="#" id="contact-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           Contact
+                           </a>
+                           <div class="dropdown-menu" aria-labelledby="contact-dropdown">
+                              <a class="dropdown-item" href="contact.php">Contact</a>
+                              
+                           </div>
+                        </li>
+                          
+					         <li class="nav-item">
+                           <a class="nav-link" href="admin.php">Admin
+                           </a>
+                        </li>
+                         
+                         <li class="nav-item">
+                           <form action="services.php" method="post">
+                           <input class="form-control" type="text" name="Petname"placeholder="Search Doctor's" aria-label="Search">
+
+                         </li>
+                        
+                          <input type="submit" class="btn btn-danger" style="margin-left: 30px;margin-bottom: 30px">
+                          </form>
+
+                   
+                     </ul>
+					 <!--/ul -->
+                  </div>
+				   <!--collapse -->
+               </div>
+			    <!-- /container -->
+            </div>
+			 <!-- /navbar -->
+         </div>
+		  <!--/row -->
+      </nav>
